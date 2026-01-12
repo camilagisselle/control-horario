28c32fe677c52c6dd4202a2c8cd0c598cf203fbd
Francisca <francisca.andrade2025@soymas.cl>
2026-01-12 14:09:52 -0300

    cambios fran

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import RecupContrasena from "./pages/Recuperarcontrasena/Recupcontrasena";
-import Registro from "./Registro";
+import Registro from "./pages/Registros/Registro";
 import Historial from "./pages/Historial/Historialpaginas";
-
+ 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/recuperar" element={<RecupContrasena />} />
-        <Route path="/registro" element={<Registro />} />
-      </Routes>
-    </Router>
+<Router>
+<Routes>
+<Route path="/" element={<Login />} />
+<Route path="/recuperar" element={<RecupContrasena />} />
+<Route path="/registro" element={<Registro />} />
+<Route path="/historial" element={<Historial />} />
+</Routes>
+</Router>
   );
 }
-
+ 
 export default App;
