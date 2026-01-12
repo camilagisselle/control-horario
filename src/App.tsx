98dc14d4b92c134783ea421b30f7d97a5263814c
Noemi <noemi.munoz2025@soymas.cl>
2026-01-12 15:39:31 -0300

    .2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login/Login";
 import RecupContrasena from "./pages/Recuperarcontrasena/Recupcontrasena";
-import Registro from "./Registro";
+import Registro from "./pages/Registro/Registro";
 import Historial from "./pages/Historial/Historialpaginas";
-import Perfil from "./pages.n/Perfil-test";
-
+import PerfilTest from "./pages/Perfil/Perfil-test";
 
 function App() {
   return (
@@ -13,10 +13,12 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/recuperar" element={<RecupContrasena />} />
         <Route path="/registro" element={<Registro />} />
-        <Route path="/perfil-test" element={<Perfil />} />
+        <Route path="/historial" element={<Historial />} />
+        <Route path="/perfil-test" element={<PerfilTest />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
---
+++ src/pages/Perfil/Perfil-test.tsx
@@ -0,0 +1,3 @@
+export default function PerfilTest() {
+  return <h1>ESTOY EN MI PERFIL</h1>;
+}
